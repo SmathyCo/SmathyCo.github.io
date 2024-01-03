fcc9b54b69809df34931d7be51a2a621f38a18b2
Sparky <145775791+MathisSparky@users.noreply.github.com>
2024-01-03 10:36:18 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function start() {
+    window.location.href = "https://discord.gg/WTTwuw843P";
+}
+function aboutme() {
+    window.location.href = "./subpage/aboutme.html";
+}
+function whatiknow() {
+    window.location.href = "./subpage/whatiknow.html";
+}
