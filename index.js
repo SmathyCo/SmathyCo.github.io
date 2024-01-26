2a241c55b559320b3377a35c635b85c71b68c6eb
Sparky <145775791+MathisSparky@users.noreply.github.com>
2024-01-25 21:02:46 -0500

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-function start() {
-    window.location.href = "https://discord.gg/WTTwuw843P";
-}
-function aboutme() {
-    window.location.href = "./subpage/aboutme.html";
-}
-function whatiknow() {
-    window.location.href = "./subpage/whatiknow.html";
-}
