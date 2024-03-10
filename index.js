ebe4be4e977aa8be6dbf00b5f59503b2005feebd
Sparky <145775791+MathisSparky@users.noreply.github.com>
2024-03-10 15:04:00 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-function goTo(link) {
-    window.location.href = link;
-}
-
-function scrollToSection(sectionId) {
-    let targetSection = document.getElementById(sectionId);
-
-    if (targetSection) {
-        window.scrollTo({
-            top: (targetSection.offsetTop - (window.innerHeight - targetSection.offsetHeight) / 2),
-            behavior: 'smooth'
-        });
-    }
+function openLink(link) {
+    window.open(link, "_blank");
 }
