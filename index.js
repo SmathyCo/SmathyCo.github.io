21fefb5abe0561f71cda54ae9c2ce989f4c6de35
Sparky <145775791+MathisSparky@users.noreply.github.com>
2024-04-16 08:26:14 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 function openLink(link) {
     window.open(link, "_blank");
 }
+
+function createPopup(message) {
+    var overlay = document.createElement('div');
+    overlay.style.position = 'fixed';
+    overlay.style.top = '0';
+    overlay.style.left = '0';
+    overlay.style.width = '100%';
+    overlay.style.height = '100%';
+    overlay.style.zIndex = '1000';
+    document.body.appendChild(overlay);
+    var popup = document.createElement('div');
+    popup.style.position = 'fixed';
+    popup.style.top = '50%';
+    popup.style.left = '50%';
+    popup.style.transform = 'translate(-50%, -50%)';
+    popup.style.background = '#ffffff';
+    popup.style.padding = '20px';
+    popup.style.border = '2px solid #000000';
+    popup.style.borderRadius = '10px';
+    popup.style.boxShadow = '0 4px 8px rgba(0, 0, 0, 0.2)';
+    popup.innerHTML = message + "<br><br><p style='text-align: center;color: black;'>Click anywhere to close.</p>";
+    document.body.appendChild(popup);
+    overlay.onclick = function() {
+        document.body.removeChild(popup);
+        document.body.removeChild(overlay);
+    };
+}
