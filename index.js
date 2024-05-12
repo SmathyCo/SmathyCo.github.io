ba93cf49a2f0d7d83f1eb3e8894b238488986da9
Sparky <145775791+MathisSparky@users.noreply.github.com>
2024-05-12 11:13:39 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -10,3 +10,25 @@ function scrollToId(id) {
     }
     window.scrollTo({ top: element.getBoundingClientRect().top + window.pageYOffset - 60, behavior: 'smooth' });
 }
+
+function toggleNavColor() {
+    let elements = document.querySelectorAll('li a');
+    let navbar = document.querySelector('nav');
+    const msElement = document.getElementById('ms');
+    function isNavbarOverMs() {
+        const navbarRect = navbar.getBoundingClientRect();
+        const msRect = msElement.getBoundingClientRect();
+        return navbarRect.top >= msRect.bottom;
+    }
+    if (!isNavbarOverMs()) {
+        elements.forEach(element => {
+            element.style.color = "white";
+        });
+    } else {
+        elements.forEach(element => {
+            element.style.color = "black";
+        });
+    }
+}
+
+window.addEventListener('scroll', toggleNavColor);
