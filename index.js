b05eac4424ee368f43e555ac7f081e638a53070f
Sparky <145775791+MathisSparky@users.noreply.github.com>
2024-05-11 23:56:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function scrollToId(id) {
     const element = document.getElementById(id);
     let url = window.location.href;
     if (!element) {
-        window.open(url.substring(0, url.indexOf("newwebsite") + "newwebsite".length) + "/index.html", "_self");
+        window.open(url.substring("./index.html", "_self"));
     }
     window.scrollTo({ top: element.getBoundingClientRect().top + window.pageYOffset - 60, behavior: 'smooth' });
 }
