c39b401ac77d43ec9b6bff7bebc13f87ab3b719b
Sparky <145775791+MathisSparky@users.noreply.github.com>
2024-07-23 21:50:48 -0400

    Add files via upload

--- learn/search.js
+++ learn/search.js
@@ -8,79 +8,79 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+function goLoading(x, y) {
+    window.location.href = x + '?redirectUrl=' + encodeURIComponent(y);
+}
+
 function search() {
-    function op(lk) {
-        window.location.href = lk;
-        return;
-    }
     let x = document.getElementById("search").value.toUpperCase().replaceAll(" ", "");
     
     if (x === "") {
         createPopup("Please insert a programming language name, if you have no idea what the programming language you want to learn is, you should ask on our Discord.");
     } else if (x === "ASSEMBLY" || x === "ASM") {
-        op("languages/assembly/assembly.html");
+        goLoading('../loading.html', './learn/languages/assembly/assembly.html');
     } else if (x === "C" || x === "CLANG") {
-        op("languages/c/c.html");
+        goLoading('../loading.html', './learn/languages/c/c.html');
     } else if (x === "C#" || x === "CS" || x === "CSHARP") {
-        op("languages/csharp/csharp.html");
+        goLoading('../loading.html', './learn/languages/csharp/csharp.html');
     } else if (x === "CSS") {
-        op("languages/css/css.html");
+        goLoading('../loading.html', './learn/languages/css/css.html');
     } else if (x === "D" || x === "DLANG") {
-        op("languages/d/d.html");
+        goLoading('../loading.html', './learn/languages/d/d.html');
     } else if (x === "DART" || x === "DT") {
-        op("languages/dart/dart.html");
+        goLoading('../loading.html', './learn/languages/dart/dart.html');
     } else if (x === "GO" || x === "GOLANG") {
-        op("languages/go/go.html");
+        goLoading('../loading.html', './learn/languages/go/go.html');
     } else if (x === "HTML") {
-        op("languages/html/html.html");
+        goLoading('../loading.html', './learn/languages/html/html.html');
     } else if (x === "JAVA" || x === "JV") {
-        op("languages/java/java.html");
+        goLoading('../loading.html', './learn/languages/java/java.html');
     } else if (x === "JAVASCRIPT" || x === "JS") {
-        op("languages/javascript/javascript.html");
+        goLoading('../loading.html', './learn/languages/javascript/javascript.html');
     } else if (x === "KOTLIN" || x === "KOT") {
-        op("languages/kotlin/kotlin.html");
+        goLoading('../loading.html', './learn/languages/kotlin/kotlin.html');
     } else if (x === "NODEJS" || x === "NODE.JS" || x === "NODE") {
-        op("languages/nodejs/nodejs.html");
+        goLoading('../loading.html', './learn/languages/nodejs/nodejs.html');
     } else if (x === "OBJECTIVEC" || x === "OBJC") {
-        op("languages/objectivec/objectivec.html");
+        goLoading('../loading.html', './learn/languages/objectivec/objectivec.html');
     } else if (x === "PERL") {
-        op("languages/perl/perl.html");
+        goLoading('../loading.html', './learn/languages/perl/perl.html');
     } else if (x === "PHP") {
-        op("languages/php/php.html");
+        goLoading('../loading.html', './learn/languages/php/php.html');
     } else if (x === "PYTHON" || x === "PY") {
-        op("languages/python/python.html");
+        goLoading('../loading.html', './learn/languages/python/python.html');
     } else if (x === "R") {
-        op("languages/r/r.html");
+        goLoading('../loading.html', './learn/languages/r/r.html');
     } else if (x === "RUBY" || x === "RB") {
-        op("languages/ruby/ruby.html");
+        goLoading('../loading.html', './learn/languages/ruby/ruby.html');
     } else if (x === "RUST" || x === "RS") {
-        op("languages/rust/rust.html");
+        goLoading('../loading.html', './learn/languages/rust/rust.html');
     } else if (x === "SCALA") {
-        op("languages/scala/scala.html");
+        goLoading('../loading.html', './learn/languages/scala/scala.html');
     } else if (x === "SCRATCH") {
-        op("languages/scratch/scratch.html");
+        goLoading('../loading.html', './learn/languages/scratch/scratch.html');
     } else if (x === "SKRIPT" || x === "SK") {
-        op("languages/skript/skript.html");
+        goLoading('../loading.html', './learn/languages/skript/skript.html');
     } else if (x === "SQL") {
-        op("languages/sql/sql.html");
+        goLoading('../loading.html', './learn/languages/sql/sql.html');
     } else if (x === "SWIFT") {
-        op("languages/swift/swift.html");
+        goLoading('../loading.html', './learn/languages/swift/swift.html');
     } else if (x === "TYPESCRIPT" || x === "TS") {
-        op("languages/typescript/typescript.html");
+        goLoading('../loading.html', './learn/languages/typescript/typescript.html');
     } else if (x === "VISUALBASIC" || x === "VB") {
-        op("languages/visualbasic/visualbasic.html");
+        goLoading('../loading.html', './learn/languages/visualbasic/visualbasic.html');
     } else if (x === "GROOVY") {
-        op("languages/groovy/groovy.html");
+        goLoading('../loading.html', './learn/languages/groovy/groovy.html');
     } else if (x === "MATLAB" || x === "ML") {
-        op("languages/matlab/matlab.html");
+        goLoading('../loading.html', './learn/languages/matlab/matlab.html');
     } else if (x === "MAKE" || x === "MK" || x === "MF" || x === "MAKEFILE") {
-        op("languages/make/make.html");
+        goLoading('../loading.html', './learn/languages/make/make.html');
     } else if (x === "SHELL" || x === "SH") {
-        op("languages/shell/shell.html");
+        goLoading('../loading.html', './learn/languages/shell/shell.html');
     } else if (x === "POWERSHELL" || x === "PS") {
-        op("languages/powershell/powershell.html");
+        goLoading('../loading.html', './learn/languages/powershell/powershell.html');
     } else if (x === "BASH") {
-        op("languages/bash/bash.html");
+        goLoading('../loading.html', './learn/languages/bash/bash.html');
     } else {
         createPopup("The programming language " + document.getElementById("search").value + " wasn't found. Try correcting errors in the name or suggest the programming language on our Discord.");
     }
